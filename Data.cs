92ba58fed8ba13639c7e2eb4b3c2e671e0d4ae5e
michael moerz <michael@moerz.co.at>
2021-06-07 00:05:12 +0200

    adding initial templates for a Validatorclass

---
+++ Data.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class $safeitemname$Data
+    {
+        private readonly ISqlDataAccess _db;
+
+        public $safeitemname$Data(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task Delete(Model data)
+        {
+            string sql = @"DELETE FROM [dbo].[]
+                           WHERE ";
+
+            return _db.Execute(sql, data);
+        }
+
+        public Task<List<Model>> Get()
+        {
+            string sql = @"SELECT * FROM [dbo].[]";
+
+            return _db.LoadData<Model, dynamic>(sql, new { });
+        }
+
+        public Task Insert(Model data)
+        {
+            string sql = @"INSERT INTO [dbo].[] ()
+                           VALUES ()";
+
+            return _db.SaveData(sql, data);
+        }
+
+        public Task Update(Model data)
+        {
+            string sql = @"UPDATE [dbo].[] SET 
+                           WHERE ";
+
+            return _db.Execute(sql, data);
+        }
+    }
+}
---
+++ FluentModelValidator/Data.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class $ModelName$Validator : AbstractValidator<$ModelName$Model>
+    {
+        public $ModelName$Validator()
+        {
+            // RuleFor(p => p.Example).NotEmpty().WithMessage("Stadt muss angegeben werden.");
+        }
+    }
+}

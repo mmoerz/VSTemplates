89885015b1c65b7784911f76a2c4909d88101257
michael moerz <michael@moerz.co.at>
2021-08-18 15:55:46 +0200

    Add - Project Template for dbdeploy

---
+++ ProjectTemplates/at.gv.bmi.bk.xxxxx.dbdeploy/Migrations/0002 Create LoT Tables.sql
@@ -0,0 +1,74 @@
+﻿GO
+CREATE TABLE [LoT].[StatusCategory] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [ValidFrom] DATETIME      NOT NULL,
+    [ValidTo]   DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE TABLE [LoT].[StatisticalCategory] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [ValidFrom] DATETIME      NOT NULL,
+    [ValidTo]   DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE TABLE [LoT].[Reporter] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [ValidFrom] DATETIME      NOT NULL,
+    [ValidTo]   DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
+GO
+CREATE TABLE [LoT].[MonitoredSubstance] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [ValidFrom] DATETIME      NOT NULL,
+    [ValidTo]   DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
+GO
+CREATE TABLE [LoT].[Measurement] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (20) NOT NULL,
+    [ValidFrom] DATETIME      NOT NULL,
+    [ValidTo]   DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
+GO
+CREATE TABLE [LoT].[Explosive] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [ValidFrom] DATETIME      NOT NULL,
+    [ValidTo]   DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE TABLE [LoT].[DrugPrecursor] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [ValidFrom] DATETIME      NOT NULL,
+    [ValidTo]   DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
---
+++ ProjectTemplates/at.gv.bmi.bk.xxxxx.dbdeploy/PostDeployment/0001 Functions/0001 Create Func name.sql
@@ -0,0 +1,9 @@
+﻿CREATE FUNCTION [dbo].[emptyFunc] ( @Id Int  )
+  RETURNS DATE
+  AS
+  BEGIN
+	  DECLARE @today DATE
+      SELECT @today = GETDATE()
+      RETURN(@today)
+  END
+GO

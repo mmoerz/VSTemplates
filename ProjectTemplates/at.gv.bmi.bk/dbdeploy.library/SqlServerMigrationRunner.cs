3de04eb8fbc4ebe8884d0a6596081029e1624d6c
michael moerz <michael@moerz.co.at>
2021-08-18 18:43:56 +0200

    Update - two projects into one didn't work, try 2

---
+++ ProjectTemplates/at.gv.bmi.bk/dbdeploy.library/SqlServerMigrationRunner.cs
@@ -0,0 +1,204 @@
+﻿using DbUp;
+using DbUp.Builder;
+using DbUp.Engine;
+using DbUp.Helpers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// A helper class for executing sql scripts embedded in an assembly having
+    /// the purpose to migrate (create/upgrade) a database.
+    /// </summary>
+    public class SqlServerMigrationRunner
+    {
+        public enum ResultType
+        {
+            PREDEPLOY,
+            ADDITIONALPREDEPLOY,
+            MIGRATION,
+            POSTDEPLOY,
+            ADDITIONALPOSTDEPLOY
+        }
+
+        /// <summary>
+        /// stores the database upgrade results of the different deployment phases.
+        /// </summary>
+        public Dictionary<ResultType, DatabaseUpgradeResult> UpgradeResults { get; private set; }
+
+        public string ConnectionString { get; set; }
+        public Assembly BaseAssembly { get; set; }
+        public Assembly AdditionalAssembly { get; set; }     
+        public Dictionary<string,string> Variables { get; set; }
+
+        // See how to use variables in your scripts: 
+        // https://dbup.readthedocs.io/en/latest/more-info/variable-substitution/
+
+        /// <summary>
+        /// class that runs the pre, migrations and post scripts.
+        /// </summary>
+        /// <param name="connectionString">connectionstring for the database to migrate</param>
+        /// <param name="baseNameSpace">if null, the pre, migration, and postscripts of the library are used</param>
+        /// <param name="additionalBaseNameSpace">path for additional scripts for pre and post migration</param>
+        /// <param name="variables">variable substitutions for the sripts</param>
+        public SqlServerMigrationRunner(string connectionString,
+            Assembly baseAssembly,
+            Assembly additionalAssembly,
+            Dictionary<string, string> variables)
+        {
+            BaseAssembly = CheckOrSetToThisLibrary(baseAssembly);
+            AdditionalAssembly = CheckOrSetToThisLibrary(additionalAssembly);
+            ConnectionString = connectionString;
+            Variables = variables;
+            UpgradeResults = new Dictionary<ResultType, DatabaseUpgradeResult>();
+
+            PrepareAppSchema();
+        }
+
+        /// <summary>
+        /// Checks that assembly is not null and if it is sets this library as the assembly.
+        /// </summary>
+        /// <param name="assembly">assembly to check for null</param>
+        /// <returns>returns assembly if not null otherwise assembly of this library.</returns>
+        private static Assembly CheckOrSetToThisLibrary(Assembly assembly)
+        {
+            if (assembly != null)
+                return assembly;
+            return Assembly.GetAssembly(typeof(UpgradeEngineBuilderExtension));
+        }
+
+        /// <summary>
+        /// DbUP doesn't create the schema anymore, so this creates [app] schema, since
+        /// [app].[Migrations] table is used to store the journal for the execution state of scripts.
+        /// </summary>
+        /// <returns>Success when app schema was properly created.</returns>
+        private DatabaseUpgradeResult PrepareAppSchema()
+        {
+             UpgradeEngineBuilder builder = DeployChanges.To
+                .SqlDatabase(ConnectionString)
+                .JournalTo(new NullJournal())
+                .WithScripts(new ScriptCheckAndCreateSchemaApp());
+            return builder.Build().PerformUpgrade();
+        }
+
+        /// <summary>
+        /// executes the scripts found in the given assembly and subdirectory.
+        /// </summary>
+        /// <param name="assembly">assembly that contains the scripts</param>
+        /// <param name="pathPostfix">subdirectory in VS containing the scripts</param>
+        /// <param name="alwaysRun">specify whether the operation should be tracked in the journal or not.</param>
+        /// <returns>result of the upgrade.</returns>
+        private DatabaseUpgradeResult RunIt(Assembly assembly, string pathPostfix, bool alwaysRun)
+        {
+            UpgradeEngineBuilder builder;
+            builder = DeployChanges.To
+                .SqlDatabase(ConnectionString)
+                .WithVariables(Variables)
+                .WithPrefixPostfixPathForEmbeddedScripts(assembly, pathPostfix)
+                .WithPredefinedJournal(alwaysRun);
+            return builder.Build().PerformUpgrade();
+        }
+
+        /// <summary>
+        /// run predeployment scripts normally found in "PreDeployment" folder.
+        /// That folder can have subdirectories containing scripts as well. 
+        /// Those will be executed as well.
+        /// </summary>
+        /// <param name="preDeploymentPostfix">name of the folder to filter for.</param>
+        /// <returns>result of the upgrade.</returns>
+        public DatabaseUpgradeResult RunPredeployment(string preDeploymentPostfix)
+        {
+            UpgradeResults.Add(
+                ResultType.PREDEPLOY, 
+                RunIt(BaseAssembly, preDeploymentPostfix, true)
+                );
+            return UpgradeResults[ResultType.PREDEPLOY];
+        }
+
+        /// <summary>
+        /// run additional predeployment scripts.
+        /// </summary>
+        /// <param name="additionalPreDeploymentPostfix">name of the folder to filter for.</param>
+        /// <returns>result of the upgrade.</returns>
+        public DatabaseUpgradeResult RunAdditionalPredeployment(string additionalPreDeploymentPostfix)
+        {
+            if (string.IsNullOrWhiteSpace(additionalPreDeploymentPostfix)) 
+                return new DatabaseUpgradeResult(new List<SqlScript>(), false, null, null);
+            UpgradeResults.Add(
+                ResultType.ADDITIONALPREDEPLOY,
+                RunIt(AdditionalAssembly, additionalPreDeploymentPostfix, true)
+            );
+            return UpgradeResults[ResultType.ADDITIONALPREDEPLOY];
+        }
+
+        /// <summary>
+        /// Run the (core) migration scripts.
+        /// That folder can have subdirectories containing scripts as well. 
+        /// Those will be executed as well.
+        /// </summary>
+        /// <param name="migrationsNamespace">name of the folder to filter for.</param>
+        /// <returns>result of the upgrade.</returns>
+        public DatabaseUpgradeResult RunMigration(string migrationsNamespace)
+        {
+            if (string.IsNullOrWhiteSpace(migrationsNamespace))
+                throw new System.ArgumentNullException(nameof(migrationsNamespace));
+
+            UpgradeResults.Add(
+                ResultType.MIGRATION,
+                RunIt(BaseAssembly, migrationsNamespace, false)
+                );
+            return UpgradeResults[ResultType.MIGRATION];
+        }
+
+        /// <summary>
+        /// Run the post deployment scripts, normally found in "PostDeployment" folder.
+        /// That folder can have subdirectories containing scripts as well. 
+        /// Those will be executed as well.
+        /// </summary>
+        /// <param name="postDeploymentPostfix">name of the folder to filter for.</param>
+        /// <returns>result of the upgrade.</returns>
+        public DatabaseUpgradeResult RunPostdeployment(string postDeploymentPostfix)
+        {
+            UpgradeResults.Add(
+                ResultType.POSTDEPLOY, 
+                RunIt(BaseAssembly, postDeploymentPostfix, true)
+            );
+            return UpgradeResults[ResultType.POSTDEPLOY];
+        }
+
+        /// <summary>
+        /// Run the additional post deployment scripts.
+        /// That folder can have subdirectories containing scripts as well. 
+        /// Those will be executed as well.
+        /// </summary>
+        /// <param name="additionalPostDeploymentPostfix">name of the folder to filter for.</param>
+        /// <returns>result of the upgrade.</returns>
+        public DatabaseUpgradeResult RunAdditionalPostdeployment(string additionalPostDeploymentPostfix)
+        {
+            if (string.IsNullOrWhiteSpace(additionalPostDeploymentPostfix)) 
+                return new DatabaseUpgradeResult(new List<SqlScript>(), false, null, null);
+            UpgradeResults.Add(
+                ResultType.ADDITIONALPREDEPLOY,
+                RunIt(AdditionalAssembly, additionalPostDeploymentPostfix, true)
+            );
+            return UpgradeResults[ResultType.ADDITIONALPREDEPLOY];
+        }
+
+        /// <summary>
+        /// Summarizes results of all executed migration steps.
+        /// </summary>
+        /// <returns>merged result of all upgrade. True on full successe, otherwise false.</returns>
+        public bool GetMergedResult()
+        {
+            bool result = true;
+            foreach (var item in UpgradeResults)
+            {
+                result &= item.Value.Successful;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProjectTemplates/at.gv.bmi.bk/dbdeploy/Program.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using DbUp.Engine;
+using at.gv.bmi.bk.DAEMeldestelle.dbdeployLibrary;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        public static ILoggerFactory LoggerFactory;
+        public static IConfigurationRoot Configuration;
+
+        /// <summary>
+        /// Default connection string name if not supplied in configuration file
+        /// </summary>
+        private const string _connectionStringNameDefault = @"Default";
+
+        // https://stackoverflow.com/questions/38114761/asp-net-core-configuration-for-net-core-console-application
+        // https://dasith.me/2020/06/08/database-project-conversion-to-migrations/
+        static int Main(string[] args)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            Configuration = SetupJsonConfigFiles(environment)
+                .SetupSecretJsonConfigFiles()
+                .Build();
+            /*
+            LoggerFactory = new LoggerFactory()
+                .AddConsole(Configuration.GetSection("Logging"))
+                .AddDebug();
+            */
+            CheckPlatform();
+            
+            var appSettings = Configuration.GetSection("AppSettings");
+
+            var connectionStringName = string.IsNullOrWhiteSpace(appSettings["ConnectionStringName"])
+                ? _connectionStringNameDefault
+                : appSettings["ConnectionStringName"];
+
+            var connectionString = Configuration.GetConnectionString(connectionStringName);
+
+            Console.WriteLine(@"DAEMeldestelleDBDeployer" +
+                              $"Version ({typeof(IConfiguration).Assembly.GetName().Version})");
+
+            // See how to use variables in your scripts: 
+            // https://dbup.readthedocs.io/en/latest/more-info/variable-substitution/
+            Console.WriteLine("\nListing variables...\n");
+            var variables = LoadVariables(appSettings);
+
+            SqlServerMigrationRunner migrationRunner = new(connectionString, null, null, variables);
+
+            return RunMigrations(appSettings, migrationRunner);
+        }
+
+        private static void CheckPlatform()
+        {
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
+                System.Runtime.InteropServices.OSPlatform.Linux))
+            {
+                Console.WriteLine("Running on Linux");
+            }
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
+                System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                Console.WriteLine("Running on Windows");
+            }
+#if Linux
+            Console.WriteLine("Really running on Linux");
+#elif Windows
+            Console.WriteLine("Running on Windows");
+#endif
+
+        }
+
+        private static int RunMigrations(IConfigurationSection appSettings, SqlServerMigrationRunner migrationRunner)
+        {
+            // Pre deployments
+            Console.WriteLine("Begin of executing pre-deployment scripts...");
+            migrationRunner.RunPredeployment("PreDeployment");
+            migrationRunner.RunAdditionalPredeployment(appSettings["AdditionalPreDeploymentNamespace"]);
+
+            // Migrations
+            Console.WriteLine("Begin of executing migration scripts...");
+            migrationRunner.RunMigration("Migrations");
+            HandleResult(migrationRunner.UpgradeResults[SqlServerMigrationRunner.ResultType.MIGRATION]);
+            
+            // Post deployments
+            Console.WriteLine("Begin of executing post-deployment scripts...");
+            migrationRunner.RunPostdeployment("PostDeployment");
+            migrationRunner.RunAdditionalPostdeployment(appSettings["AdditionalPostDeploymentNamespace"]);
+
+            return migrationRunner.GetMergedResult() ? 0 : -1;
+        }
+        
+        private static int HandleResult(DatabaseUpgradeResult result)
+        { 
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+
+        private static ConfigurationBuilder SetupJsonConfigFiles(string environment)
+        {
+            ConfigurationBuilder builder = (ConfigurationBuilder) new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+                .AddJsonFile("appsettings.json", optional: true);
+            if (environment == "Development")
+            {
+                builder
+                    .AddJsonFile(
+                        Path.Combine(AppContext.BaseDirectory,
+                            string.Format("..{0}..{0}..{0}", Path.DirectorySeparatorChar), 
+                            $"appsettings.{environment}.json"),
+                        optional: true
+                    );
+            }
+            else if (!string.IsNullOrWhiteSpace(environment))
+            {
+                builder
+                    .AddJsonFile($"appsettings.{environment}.json", optional: false);
+            }
+            return builder;
+        }
+
+        
+        private static Dictionary<string, string> LoadVariables(IConfigurationSection section)
+        {
+            Dictionary<string, string> variables = new();
+            foreach (var k in section.AsEnumerable())
+            {
+                variables.Add(k.Key, k.Value);
+                Console.WriteLine($"${k.Key}$ = \"{k.Value}\"");
+            }
+
+            return variables;
+        }
+    }
+
+    static class CfgBuilderExtension
+    {
+        public static IConfigurationBuilder SetupSecretJsonConfigFiles(this ConfigurationBuilder builder)
+        {
+#if DEBUG
+            return builder.AddUserSecrets<Program>();
+#else
+            return builder;
+#endif
+        }
+
+    }
+}
